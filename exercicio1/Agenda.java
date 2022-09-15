97fe1298e6d5e9b7bc4059ec0ce183df1e40b7b3
kauã <kauabarrostrabalho@gmail.com>
2022-09-15 01:01:01 -0300

    testando

---
+++ exercicio1/Agenda.java
@@ -0,0 +1,22 @@
+package exercicio1;
+     public class Agenda
+{   
+     private String nome;
+     private String telefone;
+
+
+    public String getNome() {
+        return nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+   
+  
+}
